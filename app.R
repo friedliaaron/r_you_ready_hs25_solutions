f50d763a41dfb855c64b5ec3cb2873ca0fe1f030
Friedli <aaron.friedli@unibe.ch>
2026-01-22 11:42:58 +0100

    chngin the names

--- app.R
+++ app.R
@@ -3,31 +3,26 @@ library(shiny)
 # Resource Path für statische Dateien (HTML Lösungen)
 addResourcePath("res", "solutions")
 
-# Hardcoded student data
+# Hardcoded student data with anonymized names
+# Note: AnMan, AnMat, AnSte, and AnStu use 5 letters to ensure uniqueness.
 student_data <- data.frame(
   id = rep(1:26, each = 2),
   name = c(
-    "Chiara Aurora Barbaca", "Janine Andermatt", "Emilie Chenaux", "Emilia Laura Banzer",
-    "Fabienne Durand", "Tobias Biedermann", "Bastien Egli", "Monja Leandra Burri",
-    "Nadine Federer", "Anja Sabina Fend", "Moira Johanna Frey", "Nicolas Simon Gabl",
-    "Emilia Luise Knop", "Michele Alessia Hollenstein", "Oliver Gabriel Kofler", "Leila Ibraimi",
-    "Irma Despina Kurth", "Mara Jaeggi", "Janis Noah Luginbuehl", "Laura Angela Jaeggi",
-    "Annika Eva Mantsch", "Ece Aysu Kirac", "Madina Marti", "Amelie Kuehn",
-    "Julian Merlin Meier", "Noemi Cristina Martin Tejedor", "Helen Meier", "Anna Julia Matt",
-    "Annina Mirjam Meister", "Arulini Murugavel", "Noel Louis Niffeler", "Ridha Puran",
-    "Huelya Oezmen", "Carla Rentsch", "Roberta Santoli", "Thomas Schneider",
-    "Svenja Victoria Schaerer", "Nic Sobhani", "Lukas Andrea Scheurer", "Ann Jolyne Stettler",
-    "Florence Carina Medea Stoffel", "Natasa Stojanovic", "Anna Sophie Stulz", "Elina Beata Isabella Stoop",
-    "Irina Ana Tadic", "Isabelle Alexa Tscharner", "Simona Cristina Thoeny", "Alexandra Danielle Waelti",
-    "Rea Vollmer", "Yosiga Yogendran", "Katsiaryna Zengin", "Nadine Zuberbuehler"
+    "ChBa", "JaAn", "EmCh", "EmBa", "FaDu", "ToBi", "BaEg", "MoBu",
+    "NaFe", "AnFe", "MoFr", "NiGa", "EmKn", "MiHo", "OlKo", "LeIb",
+    "IrKu", "MaJa", "JaLu", "LaJa", "AnMan", "EcKi", "MaMa", "AmKu",
+    "JuMe", "NoTe", "HeMe", "AnMat", "AnMe", "ArMu", "NoNi", "RiPu",
+    "HuOe", "CaRe", "RoSa", "ThSc", "SvSc", "NiSo", "LuSc", "AnSte",
+    "FlSt", "NaSt", "AnStu", "ElSt", "IrTa", "IsTs", "SiTh", "AlWa",
+    "ReVo", "YoYo", "KaZe", "NaZu"
   ), stringsAsFactors = FALSE
 )
 
+# Create named vector for selection (Name = ID)
 student_choices <- setNames(student_data$id, student_data$name)
 student_choices <- student_choices[order(names(student_choices))]
 
 ui <- fluidPage(
-  # Titel mit Logo aus dem www-Ordner
   titlePanel(
     windowTitle = "Musterlösungen R you Ready HS25",
     title = div(
@@ -39,6 +34,9 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
+      helpText(HTML("<b>Identifikation:</b> Suchen Sie nach den ersten zwei Buchstaben Ihres Vornamens und Nachnamens (z.B. <i>Max Mustermann</i> &rarr; <b>MaMu</b>). <br><br><i>Hinweis: Bei Namensdoppelungen wurden drei Buchstaben des Nachnamens verwendet (z.B. MaMus).</i>")),
+      br(),
+      
       selectInput("id", "Name auswählen:", 
                   choices = c("Bitte wählen..." = "", student_choices),
                   selectize = FALSE), 
@@ -62,14 +60,12 @@ ui <- fluidPage(
   
   hr(),
   
-  # Hinweis-Box am Ende
   div(
     style = "background-color: #f9f9f9; border: 1px solid #ddd; padding: 20px; border-radius: 5px; margin: 20px 0;",
     h4(strong("Wichtiger Hinweis zur Musterlösung")),
     p("Dies ist eine von mehreren möglichen Lösungen für die Analysis- und Processing-Skripte. Da die Berichte individuell an Ihren spezifischen Datensatz angepasst wurden, bedeuten Abweichungen von dieser Vorlage ", 
       strong("nicht,"), " dass Ihr Code oder Ihre Ergebnisse falsch sind."),
-    p("Das zugrunde liegende Skript ist parametrisiert, um alle 52 Datensätze abzudecken. Daher sind einige Code-Abschnitte im Bericht ausgeblendet oder werden nicht ausgeführt. 
-       Der vollständige Code kann über die Schaltfläche ", strong("„</> Code“"), " (oben rechts) eingesehen werden. Bitte beachten Sie, dass diese zusätzlichen Teile primär der technischen Veranschaulichung dienen.")
+    p("Der vollständige Code kann über die Schaltfläche ", strong("„</> Code“"), " (oben rechts) eingesehen werden.")
   )
 )
 
